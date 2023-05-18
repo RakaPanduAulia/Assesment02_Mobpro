9a7e552dc4feaa98654057ecdfbc23b7a1f97699
Pandu <rakapandu12@gmail.com>
2023-05-18 20:24:39 +0700

    List History dan tombol delete(Eror)

--- app/src/main/java/org/d3if3068/assesment001/HistoryActivity.kt
+++ app/src/main/java/org/d3if3068/assesment001/HistoryActivity.kt
@@ -1,65 +1,46 @@
-package org.d3if3068.assesment001;
+package org.d3if3068.assesment001
 
-import android.database.Cursor;
-import android.os.Bundle;
-import android.widget.Toast;
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import java.util.ArrayList;
-
-public class HistoryActivity extends AppCompatActivity {
-
-    RecyclerView recyclerView;
-
-    MyDatabaseHelper myDB;
-    ArrayList<String> history_id, history_celcius, history_fahrenheit;
-    CustomAdapter customAdapter;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_history);
-
-        recyclerView = findViewById(R.id.recyclerView);
-
-        myDB = new MyDatabaseHelper(HistoryActivity.this);
-        history_id = new ArrayList<>();
-        history_celcius = new ArrayList<>();
-        history_fahrenheit = new ArrayList<>();
-
-        storeDataInArrays();
-
-        customAdapter = new CustomAdapter(HistoryActivity.this, history_id, history_celcius, history_fahrenheit);
-        recyclerView.setAdapter(customAdapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(HistoryActivity.this));
-
+class HistoryActivity : AppCompatActivity() {
+    var recyclerView: RecyclerView? = null
+    var myDB: MyDatabaseHelper? = null
+    var history_id: ArrayList<String>? = null
+    var history_celcius: ArrayList<String>? = null
+    var history_fahrenheit: ArrayList<String>? = null
+    var customAdapter: CustomAdapter? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_history)
+        recyclerView = findViewById(R.id.recyclerView)
+        myDB = MyDatabaseHelper(this@HistoryActivity)
+        history_id = ArrayList()
+        history_celcius = ArrayList()
+        history_fahrenheit = ArrayList()
+        storeDataInArrays()
+        customAdapter = CustomAdapter(
+            this@HistoryActivity,
+            history_id!!,
+            history_celcius!!,
+            history_fahrenheit!!
+        )
+        recyclerView.setAdapter(customAdapter)
+        recyclerView.setLayoutManager(LinearLayoutManager(this@HistoryActivity))
     }
-    void storeDataInArrays(){
-        Cursor cursor = myDB.readAllData();
-        if(cursor.getCount() == 0){
-            Toast.makeText(this, "No data", Toast.LENGTH_SHORT).show();
-        }else{
-            while (cursor.moveToNext()){
-                history_id.add(cursor.getString(0));
-                history_celcius.add(cursor.getString(1));
-                history_fahrenheit.add(cursor.getString(2));
+
+    fun storeDataInArrays() {
+        val cursor = myDB!!.readAllData()
+        if (cursor.count == 0) {
+            Toast.makeText(this, "No data", Toast.LENGTH_SHORT).show()
+        } else {
+            while (cursor.moveToNext()) {
+                history_id!!.add(cursor.getString(0))
+                history_celcius!!.add(cursor.getString(1))
+                history_fahrenheit!!.add(cursor.getString(2))
             }
         }
-    }
-    // Tambahkan method deleteData()
-//    void deleteData(int position) {
-//        int result = myDB.deleteData(history_id.get(position));
-//        if (result > 0) {
-//            Toast.makeText(this, "Data deleted successfully", Toast.LENGTH_SHORT).show();
-//            history_id.remove(position);
-//            history_celcius.remove(position);
-//            history_fahrenheit.remove(position);
-//            customAdapter.notifyItemRemoved(position);
-//        } else {
-//            Toast.makeText(this, "Failed to delete data", Toast.LENGTH_SHORT).show();
-//        }
-//    }
 }
--- app/src/main/java/org/d3if3068/assesment001/MyDatabaseHelper.kt
+++ app/src/main/java/org/d3if3068/assesment001/MyDatabaseHelper.kt
@@ -1,74 +1,62 @@
-package org.d3if3068.assesment001;
+package org.d3if3068.assesment001
 
-import android.content.ContentValues;
-import android.content.Context;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
-import android.widget.Toast;
+import android.content.ContentValues
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.widget.Toast
 
-import androidx.annotation.Nullable;
-
-class MyDatabaseHelper extends SQLiteOpenHelper {
-
-    private Context context;
-    private final static String DATABASE_NAME = "HistoryDatabase.db";
-    private final static int DATABASE_VERSION = 1;
-
-    private static final String TABLE_NAME = "history";
-    private static final String COLUMN_ID = "_id";
-    private static final String COLUMN_CELCIUS = "celcius";
-    private static final String COLUMN_FAHRENHEIT = "fahrenheit";
-
-    public MyDatabaseHelper(@Nullable Context context) {
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
-        this.context = context;
-    }
-
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-        String query = "CREATE TABLE " + TABLE_NAME +
+class MyDatabaseHelper(private val context: Context?) : SQLiteOpenHelper(
+    context, DATABASE_NAME, null, DATABASE_VERSION
+) {
+    override fun onCreate(db: SQLiteDatabase) {
+        val query = "CREATE TABLE " + TABLE_NAME +
                 " (" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
                 COLUMN_CELCIUS + " TEXT, " +
-                COLUMN_FAHRENHEIT + " TEXT);";
-        db.execSQL(query);
+                COLUMN_FAHRENHEIT + " TEXT);"
+        db.execSQL(query)
     }
 
-    @Override
-    public void onUpgrade(SQLiteDatabase db,int i,int i1) {
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
-        onCreate(db);
+    override fun onUpgrade(db: SQLiteDatabase, i: Int, i1: Int) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME)
+        onCreate(db)
     }
 
-    void addHistory(String celcius, String fahrenheit){
-        SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues cv = new ContentValues();
-
-        cv.put(COLUMN_CELCIUS, celcius);
-        cv.put(COLUMN_FAHRENHEIT, fahrenheit);
-
-        long result = db.insert(TABLE_NAME, null, cv);
-
-        if(result == -1){
-            Toast.makeText(context, "Failed", Toast.LENGTH_SHORT).show();
-        }else {
-            Toast.makeText(context, "Success", Toast.LENGTH_SHORT).show();
+    fun addHistory(celcius: String?, fahrenheit: String?) {
+        val db = this.writableDatabase
+        val cv = ContentValues()
+        cv.put(COLUMN_CELCIUS, celcius)
+        cv.put(COLUMN_FAHRENHEIT, fahrenheit)
+        val result = db.insert(TABLE_NAME, null, cv)
+        if (result == -1L) {
+            Toast.makeText(context, "Failed", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(context, "Success", Toast.LENGTH_SHORT).show()
         }
     }
-    Cursor readAllData(){
-        String query = "SELECT * FROM " + TABLE_NAME;
-        SQLiteDatabase db = this.getReadableDatabase();
 
-        Cursor cursor = null;
-        if(db != null){
-            cursor = db.rawQuery(query,null);
+    fun readAllData(): Cursor? {
+        val query = "SELECT * FROM " + TABLE_NAME
+        val db = this.readableDatabase
+        var cursor: Cursor? = null
+        if (db != null) {
+            cursor = db.rawQuery(query, null)
         }
-        return cursor;
+        return cursor
     }
 
-    // Tambahkan method deleteData()
-//    int deleteData(String id) {
-//        SQLiteDatabase db = this.getWritableDatabase();
-//        return db.delete(TABLE_NAME, COLUMN_ID + "=?", new String[]{id});
-//    }
+    fun deleteDataById(id: String): Int {
+        val db = this.writableDatabase
+        return db.delete(TABLE_NAME, COLUMN_ID + "=?", arrayOf(id))
+    }
+
+    companion object {
+        private const val DATABASE_NAME = "HistoryDatabase.db"
+        private const val DATABASE_VERSION = 1
+        private const val TABLE_NAME = "history"
+        private const val COLUMN_ID = "_id"
+        private const val COLUMN_CELCIUS = "celcius"
+        private const val COLUMN_FAHRENHEIT = "fahrenheit"
+    }
 }
